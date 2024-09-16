a7697f88cdc8c5e157b67afc16eb725c9c1a65f0
mimy-GMC <144010948+mimy-GMC@users.noreply.github.com>
2024-09-16 13:47:05 +0200

    modification

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 import { useState, useCallback, useRef, useEffect } from "react";
-import { MapContainer, TileLayer, Marker, Popup, useMapEvents, useMap } from 'react-leaflet';
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-import MarkerClusterGroup from 'react-leaflet-cluster';
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+  useMap,
+} from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import MarkerClusterGroup from "react-leaflet-cluster";
 import ListComplexe from "./components/list-complexe";
 
 import Header from "./components/header";
-import './App.css';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import "./App.css";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 // Correction pour les icônes
 
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: import('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: import('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: import('leaflet/dist/images/marker-shadow.png'),
+  iconRetinaUrl: import("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: import("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: import("leaflet/dist/images/marker-shadow.png"),
 });
 
 function App() {
@@ -25,20 +32,18 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [progress, setProgress] = useState(0);
   const [lastBounds, setLastBounds] = useState(null);
-  const [searchItem, setSearchItem] = useState('');
+  const [searchItem, setSearchItem] = useState("");
   const [filteredComplexe, setFilteredComplexe] = useState([]);
   const [selectedRegion, setSelectedRegion] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 10; 
+  const itemsPerPage = 10;
   const timeoutRef = useRef(null);
-  const defaultBounds = [41.0, -5.0, 51.0, 9.0]; 
+  const defaultBounds = [41.0, -5.0, 51.0, 9.0];
 
   const defaultCenter = [46.603354, 1.888334]; // Center of France
   const defaultZoom = 6; // Default zoom level
   const [selectedComplexe, setSelectedComplexe] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  
-
 
   const fetchComplexes = useCallback(async (bounds, limit = 1000) => {
     setIsLoading(true);
@@ -48,14 +53,24 @@ function App() {
 
     try {
       while (true) {
-        const response = await fetch(`https://equipements.sports.gouv.fr/api/explore/v2.1/catalog/datasets/data-es/records?limit=100&offset=${offset}&refine=inst_part_type_filter%3A%22Complexe%20sportif%22&geofilter.bbox=${bounds.join(',')}`);
-        if (!response.ok) throw new Error(`Error ${response.status}, ${response.statusText}`);
+        const response = await fetch(
+          `https://equipements.sports.gouv.fr/api/explore/v2.1/catalog/datasets/data-es/records?limit=100&offset=${offset}&refine=inst_part_type_filter%3A%22Complexe%20sportif%22&geofilter.bbox=${bounds.join(
+            ","
+          )}`
+        );
+        if (!response.ok)
+          throw new Error(`Error ${response.status}, ${response.statusText}`);
         const responseData = await response.json();
 
         if (responseData.results && Array.isArray(responseData.results)) {
           const newComplexes = responseData.results
-            .filter(result => result.coordonnees && result.coordonnees.lat && result.coordonnees.lon)
-            .map(result => {
+            .filter(
+              (result) =>
+                result.coordonnees &&
+                result.coordonnees.lat &&
+                result.coordonnees.lon
+            )
+            .map((result) => {
               return {
                 id: result.inst_numero,
                 nom: result.inst_nom,
@@ -64,22 +79,26 @@ function App() {
                 latitude: result.coordonnees.lat,
                 longitude: result.coordonnees.lon,
                 region: result.reg_nom,
-                status : result.inst_actif,
-                sport : result.equip_type_name,
+                status: result.inst_actif,
+                sport: result.equip_type_name,
               };
             });
 
           allComplexes = [...allComplexes, ...newComplexes];
-          setData(prevData => [...prevData, ...newComplexes]);
+          setData((prevData) => [...prevData, ...newComplexes]);
           setProgress(Math.min(100, (allComplexes.length / limit) * 100));
 
-          if (responseData.results.length < 100 || allComplexes.length >= limit) break;
+          if (responseData.results.length < 100 || allComplexes.length >= limit)
+            break;
           offset += 100;
         } else {
           break;
         }
       }
-      setCachedData(prevCache => [...prevCache, { bounds, complexes: allComplexes }]);
+      setCachedData((prevCache) => [
+        ...prevCache,
+        { bounds, complexes: allComplexes },
+      ]);
     } catch (err) {
       console.error("Erreur de requête :", err);
       setErrorInfo(err.message);
@@ -88,20 +107,24 @@ function App() {
     }
   }, []);
 
-
-  
-  
   function MapEventHandler() {
     const map = useMapEvents({
       moveend: () => {
         const bounds = map.getBounds();
-        const newBounds = [bounds.getSouth(), bounds.getWest(), bounds.getNorth(), bounds.getEast()];
+        const newBounds = [
+          bounds.getSouth(),
+          bounds.getWest(),
+          bounds.getNorth(),
+          bounds.getEast(),
+        ];
 
-        const isAlreadyFetched = cachedData.some(cached => {
+        const isAlreadyFetched = cachedData.some((cached) => {
           const cachedBounds = cached.bounds;
           return (
-            newBounds[0] >= cachedBounds[0] && newBounds[1] >= cachedBounds[1] &&
-            newBounds[2] <= cachedBounds[2] && newBounds[3] <= cachedBounds[3]
+            newBounds[0] >= cachedBounds[0] &&
+            newBounds[1] >= cachedBounds[1] &&
+            newBounds[2] <= cachedBounds[2] &&
+            newBounds[3] <= cachedBounds[3]
           );
         });
 
@@ -122,15 +145,17 @@ function App() {
     const map = useMap();
     useEffect(() => {
       if (region && map) {
-        const regionBounds = data.filter(complexe => complexe.region === region).map(complexe => [complexe.latitude, complexe.longitude]);
+        const regionBounds = data
+          .filter((complexe) => complexe.region === region)
+          .map((complexe) => [complexe.latitude, complexe.longitude]);
 
         if (regionBounds.length > 0) {
           const bounds = L.latLngBounds(regionBounds);
           map.fitBounds(bounds);
         }
-      // } else if (!region && map) {
-      //   // Revenir au centre et au zoom par défaut
-      //   map.setView(defaultCenter, defaultZoom);
+        // } else if (!region && map) {
+        //   // Revenir au centre et au zoom par défaut
+        //   map.setView(defaultCenter, defaultZoom);
       }
     }, [region, map, data, defaultCenter, defaultZoom]);
 
@@ -139,7 +164,7 @@ function App() {
 
   useEffect(() => {
     if (!lastBounds) {
-      setLastBounds(defaultBounds); 
+      setLastBounds(defaultBounds);
       fetchComplexes(defaultBounds);
     }
   }, [fetchComplexes, lastBounds]);
@@ -162,111 +187,144 @@ function App() {
     let filteredItems = data;
 
     if (region) {
-      filteredItems = filteredItems.filter(complexe => complexe.region === region);
+      filteredItems = filteredItems.filter(
+        (complexe) => complexe.region === region
+      );
     }
 
     if (searchTerm) {
-      filteredItems = filteredItems.filter(complexe =>
-        complexe.nom.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        complexe.name.toLowerCase().includes(searchTerm.toLowerCase())
+      filteredItems = filteredItems.filter(
+        (complexe) =>
+          complexe.nom.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          complexe.name.toLowerCase().includes(searchTerm.toLowerCase())
       );
     }
 
     setFilteredComplexe(filteredItems);
-    setCurrentPage(1); 
+    setCurrentPage(1);
   };
 
-  const uniqueRegions = [...new Set(data.map(complexe => complexe.region))];
+  const uniqueRegions = [...new Set(data.map((complexe) => complexe.region))];
 
   const indexOfLastComplexe = currentPage * itemsPerPage;
   const indexOfFirstComplexe = indexOfLastComplexe - itemsPerPage;
-  const currentComplexes = filteredComplexe.length > 0 
-    ? filteredComplexe.slice(indexOfFirstComplexe, indexOfLastComplexe) 
-    : data.slice(indexOfFirstComplexe, indexOfLastComplexe);
+  const currentComplexes =
+    filteredComplexe.length > 0
+      ? filteredComplexe.slice(indexOfFirstComplexe, indexOfLastComplexe)
+      : data.slice(indexOfFirstComplexe, indexOfLastComplexe);
 
-  const handleNextPage = () => setCurrentPage(prevPage => prevPage + 1);
-  const handlePrevPage = () => setCurrentPage(prevPage => (prevPage > 1 ? prevPage - 1 : 1));
+  const handleNextPage = () => setCurrentPage((prevPage) => prevPage + 1);
+  const handlePrevPage = () =>
+    setCurrentPage((prevPage) => (prevPage > 1 ? prevPage - 1 : 1));
 
   return (
-
     <main>
-    <Header searchResults={filteredComplexe} />
-  
-    <div className="app-container">
-      <h1 style={{ alignItems :"center", marginLeft :"40px"}}>Recherchez des complexes sportifs .............</h1>
-  
-      <div className="search-filter">
-        
+      {/*<Header searchResults={filteredComplexe} />*/}
+      <Header
+        searchResults={filteredComplexe}
+        handleInputChange={handleInputChange}
+        uniqueRegions={uniqueRegions}
+        selectedRegion={selectedRegion}
+        handleRegionChange={handleRegionChange}
+      />
 
-        <Row>
-        <Col md={4}>
-        <input style={ {height : "35px",width :"400px"} }
-          type="text"
-          value={searchItem}
-          onChange={handleInputChange}
-          placeholder="Rechercher un complexe sportif"
-        />
-        
-        </Col>
-        <Col md={{ span: 4, offset: 4 }}>
-        <select id="region-select" onChange={handleRegionChange} value={selectedRegion || ''}>
-          <option value=""> Choisissez une region </option>
-          {uniqueRegions.map((region, index) => (
-            <option key={index} value={region}>{region}</option>
-          ))}
-        </select>
-        </Col>
-      </Row>
-        
-      </div>
-  
-      {isLoading && (
-        <div>
-          <p>Chargement en cours...</p>
-          <progress value={progress} max="100" />
+      <div className="app-container">
+        <h1 style={{ alignItems: "center", marginLeft: "40px" }}>
+          Recherchez des complexes sportifs .............
+        </h1>
+
+        <div className="search-filter">
+          <Row>
+            <Col md={4}>
+              <input
+                style={{ height: "35px", width: "400px" }}
+                type="text"
+                value={searchItem}
+                onChange={handleInputChange}
+                placeholder="Rechercher un complexe sportif"
+              />
+            </Col>
+            <Col md={{ span: 4, offset: 4 }}>
+              <select
+                id="region-select"
+                onChange={handleRegionChange}
+                value={selectedRegion || ""}
+              >
+                <option value=""> Choisissez une region </option>
+                {uniqueRegions.map((region, index) => (
+                  <option key={index} value={region}>
+                    {region}
+                  </option>
+                ))}
+              </select>
+            </Col>
+          </Row>
         </div>
-      )}
-  
-    <div className="content-wrapper">
-      <div className="list-container mb-4" >
-      <ul>
-        {currentComplexes.map((complexe, index) => (
-          <ListComplexe key={index} complexe={complexe} />
-        ))}
-      </ul>
-      </div>
-      <div className="map-container">
-          <MapContainer  center={defaultCenter} zoom={defaultZoom} style={{ height: "250px", width: "100%", marginRight : "4px" }}>
-            <TileLayer
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
-            />
-            <MapEventHandler />
-            <ZoomToRegion region={selectedRegion} defaultCenter={defaultCenter} defaultZoom={defaultZoom} />
-            <MarkerClusterGroup chunkedLoading>
-              {data.map((complexe, index) => (
-                <Marker key={index} position={[complexe.latitude, complexe.longitude]}>
-                  <Popup>
-                    <b>{complexe.name}</b><br />
-                    {complexe.nom}<br />
-                    {complexe.adresse}
-                  </Popup>
-                </Marker>
-              ))}
-            </MarkerClusterGroup>
-          </MapContainer>
-      </div>
-    </div>
 
-    <div  className="pagination" >
-          <button onClick={handlePrevPage} disabled={currentPage === 1}>Précédent</button>
-          <button onClick={handleNextPage} disabled={indexOfLastComplexe >= filteredComplexe.length}>Suivant</button>
-     </div>
-    </div>
-    
-  </main>
-  
-  
+        {isLoading && (
+          <div>
+            <p>Chargement en cours...</p>
+            <progress value={progress} max="100" />
+          </div>
+        )}
+
+        <div className="content-wrapper">
+          <div className="list-container mb-4">
+            <ul>
+              {currentComplexes.map((complexe, index) => (
+                <ListComplexe key={index} complexe={complexe} />
+              ))}
+            </ul>
+          </div>
+          <div className="map-container">
+            <MapContainer
+              center={defaultCenter}
+              zoom={defaultZoom}
+              style={{ height: "250px", width: "100%", marginRight: "4px" }}
+            >
+              <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
+              />
+              <MapEventHandler />
+              <ZoomToRegion
+                region={selectedRegion}
+                defaultCenter={defaultCenter}
+                defaultZoom={defaultZoom}
+              />
+              <MarkerClusterGroup chunkedLoading>
+                {data.map((complexe, index) => (
+                  <Marker
+                    key={index}
+                    position={[complexe.latitude, complexe.longitude]}
+                  >
+                    <Popup>
+                      <b>{complexe.name}</b>
+                      <br />
+                      {complexe.nom}
+                      <br />
+                      {complexe.adresse}
+                    </Popup>
+                  </Marker>
+                ))}
+              </MarkerClusterGroup>
+            </MapContainer>
+          </div>
+        </div>
+
+        <div className="pagination">
+          <button onClick={handlePrevPage} disabled={currentPage === 1}>
+            Précédent
+          </button>
+          <button
+            onClick={handleNextPage}
+            disabled={indexOfLastComplexe >= filteredComplexe.length}
+          >
+            Suivant
+          </button>
+        </div>
+      </div>
+    </main>
   );
 }
 
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+/*import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 function Header({ searchResults }) {
   return (
@@ -14,6 +14,44 @@ function Header({ searchResults }) {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
       </Container>
     </Navbar>
+  );
+}
+
+export default Header;*/
+import logo from "../assets/logo-bootcamp.png";
+function Header({ searchResults }) {
+  return (
+    <header
+      className="flex justify-between items-center 
+    text-black py-6 px-8 md:px-32 bg-white drop-shadow-md"
+    >
+      <a href="/">
+        <img
+          src={logo}
+          alt=""
+          className="w-52 hover:scale-105 transition-all"
+        />
+      </a>
+
+      <ul className="hidden xl:flex items-center gap-12 font-semibold text-base">
+        <li className="p-3 hover:bg-sky-400 hover:text-white rounded-md transition-all cursor-pointeur">
+          <a>Accueil</a>
+        </li>
+        <li>
+          <a>Services</a>
+        </li>
+        <li>
+          <a>Contact</a>
+        </li>
+        <li>
+          <a className="p-3 hover:bg-sky-400 hover:text-white rounded-md transition-all">
+            A propos
+          </a>
+        </li>
+      </ul>
+
+      <div className="relative hidden md:flex items-center justify-center gap-3"></div>
+    </header>
   );
 }
 
